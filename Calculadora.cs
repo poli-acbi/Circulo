d004a4822f1781b0720a2a038eba927cdf280023
poli-acbi <83432441+poli-acbi@users.noreply.github.com>
2021-09-15 20:21:52 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculoCircunferencia
+{
+    public class Calculadora
+
+    {
+        public double Pi = 3.14;
+
+        public double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace calculoCircunferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+            Console.WriteLine("Insira o valor do raio");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
